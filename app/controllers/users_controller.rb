ed0c3a7a579d4daa2bb702e50d0f64d1e4f05f1a
jasmo2 <jasmo2.com@gmail.com>
2015-04-25 14:39:24 -0500

    #2 update users routes restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
-
+    if user_signed_in?
+      render "index"
+    else
+      render "landing"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-
-  get 'scores/create'
-
-  root 'users#index'
-  get 'users/game'
   devise_for :users
+  root 'users#index'
+
+  authenticated :user do
+
+    get 'users/game'
+    post 'scores/create'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
