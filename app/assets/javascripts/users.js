3d034a216168429d83a1c4ac64b81cb702a0862b
heyjoeb <heyjoeb@hotmail.com>
2015-04-24 23:59:51 -0500

    #3 timer

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,6 +8,31 @@ $( document ).ready(function() {
 			'max': [ 10000 ]
 		}
 	});
+
+  // game countdown
+  (function(){
+    var counter = 120;
+    var span = document.getElementById("game-timer-numbers");
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span.innerHTML = getTime(counter);
+      }
+      // Display 'counter' wherever you want to display it.
+      if (counter === 0) {
+          alert('el tiempo ha terminado');
+          clearInterval(counter);
+      }
+      
+    }, 1000);
+  })();
+  var getTime = function(sg) {
+    var minutes = Math.floor(sg / 60);
+    var leftover = sg - minutes * 60;
+    leftover = (leftover < 10) ? "0" + leftover: leftover
+    return (minutes + ':' + leftover);
+  };
+
 });
 
 
