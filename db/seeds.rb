931c64cb3fcee2981d79a6133108eef2c1ee5ccc
heyjoeb <heyjoeb@hotmail.com>
2015-04-25 19:25:50 -0500

    repopular seeds, integración datos en tablas de scores

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 prng = Random.new()
-
 for iteration in 1...22
   f_name = Faker::Name.name
   f_email = Faker::Internet.email
   f_text = Faker::Lorem.paragraph(2)
   # prng.rand(0...4)
-  User.create(username: f_name, email: f_email, password: "12345678")
+  if iteration == 1 then f_name = "heyjoe" else f_name = f_name end
+  actual_user = User.create(username: f_name, email: f_email, password: "12345678")
   Game.create(textGame: f_text)
-  Score.create(score: prng.rand(0...100), user_id: prng.rand(1..22))
+  for iteration in 1...22
+    Score.create(score: prng.rand(0...100), user_id: actual_user.id)
+  end
 end
 
